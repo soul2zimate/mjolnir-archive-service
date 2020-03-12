c6e74a3bd46bcd44066a76292c329a6d7d75248f
Lukas Vydra <lvydra@redhat.com>
2020-03-12 09:50:02 +0100

    Get rid of getters and setter and move parameters directly to send method

--- core/src/main/java/org/jboss/set/mjolnir/archive/mail/MailingService.java
+++ core/src/main/java/org/jboss/set/mjolnir/archive/mail/MailingService.java
@@ -11,15 +11,10 @@ import javax.mail.internet.*;
 @ApplicationScoped
 public class MailingService {
 
-    private String toAddress;
-    private String fromAddress;
-    private String subject;
-    private String body;
-
     @Resource (mappedName="java:jboss/mail/Default")
     private Session mailSession;
 
-    public void send() throws MessagingException {
+    public void send(String fromAddress, String toAddress, String subject, String body) throws MessagingException {
         MimeBodyPart mailMessage = new MimeBodyPart();
         mailMessage.setContent(body, "text/html; charset=utf-8");
 
@@ -33,37 +28,5 @@ public class MailingService {
         message.setContent(mailContent);
 
         Transport.send(message);
-    }
-
-    public String getToAddress() {
-        return toAddress;
-    }
-
-    public void setToAddress(String toAddress) {
-        this.toAddress = toAddress;
-    }
-
-    public String getFromAddress() {
-        return fromAddress;
-    }
-
-    public void setFromAddress(String fromAddress) {
-        this.fromAddress= fromAddress;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
     }
 }
