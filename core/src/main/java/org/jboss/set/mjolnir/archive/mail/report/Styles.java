f6bd4821ac575d4e2be19ee94e83664ba084a7ad
Tomas Hofman <thofman@redhat.com>
2020-04-22 13:30:24 +0200

    Email report formatting

---
+++ core/src/main/java/org/jboss/set/mjolnir/archive/mail/report/Styles.java
@@ -0,0 +1,17 @@
+package org.jboss.set.mjolnir.archive.mail.report;
+
+public final class Styles {
+
+    static final String TABLE_STYLE = "min-width:600px;";
+    static final String TH_STYLE = "padding: 0 15px;";
+    static final String TD_STYLE = "border: 1px solid black; border-collapse: collapse; padding: 0 15px;";
+    static final String BORDER_STYLE = "border: 1px solid black; border-collapse: collapse;";
+    static final String FONT_SUCCESS_STYLE = "color:green;";
+    static final String FONT_ERROR_STYLE = "color:red;";
+    static final String H2_STYLE = "margin-bottom: 0; padding-bottom: 0;";
+    static final String SUB_HEADING_STYLE = "margin-top: 0; padding-top: 0;";
+    static final String UL_STYLE = "margin-bottom: 0; padding-bottom: 0;";
+
+    private Styles() {
+    }
+}
